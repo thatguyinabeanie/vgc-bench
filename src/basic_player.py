7aef60ea324fb2a5910f1987cafab2a6ebcbada0
Cameron Angliss <cameronangliss99@gmail.com>
2024-06-11 22:36:13 -0700

    first commit

---
+++ src/basic_player.py
@@ -0,0 +1,15 @@
+from poke_env.environment import AbstractBattle, Battle, DoubleBattle
+from poke_env.player import BattleOrder, Player
+
+
+class BasicPlayer(Player):
+    def __init__(self):
+        super().__init__()
+
+    def choose_move(self, battle: AbstractBattle) -> BattleOrder:
+        if isinstance(battle, Battle):
+            return self.choose_random_singles_move(battle)
+        elif isinstance(battle, DoubleBattle):
+            return self.choose_random_doubles_move(battle)
+        else:
+            raise Exception("Must be single or double battle")
---
+++ src/train.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from poke_env import cross_evaluate
+from poke_env.player import RandomPlayer
+
+from src.basic_player import BasicPlayer
+
+
+async def train():
+    player = BasicPlayer()
+    opponent = RandomPlayer()
+    result = await cross_evaluate([player, opponent], n_challenges=10)
+    print(result)
+
+
+if __name__ == "__main__":
+    asyncio.run(train())
