4f2bca69cbc40cec3e471e769a1074e29dcf9619
Cameron Angliss <cameronangliss99@gmail.com>
2024-06-19 01:19:13 -0700

    change format to gen4 randoms

--- src/train.py
+++ src/train.py
@@ -5,50 +5,12 @@ from poke_env.player import MaxBasePowerPlayer, RandomPlayer, SimpleHeuristicsPl
 
 from agent import Agent
 
-sample_team = """
-Gengar
-- Hypnosis
-- Thunderbolt
-- Night Shade
-- Explosion
-
-Chansey
-- Ice Beam
-- Thunderbolt
-- Thunder Wave
-- Soft-Boiled
-
-Snorlax
-- Body Slam
-- Earthquake
-- Hyper Beam
-- Self-Destruct
-
-Exeggutor
-- Sleep Powder
-- Stun Spore
-- Psychic
-- Explosion
-
-Tauros
-- Body Slam
-- Earthquake
-- Hyper Beam
-- Blizzard
-
-Lapras
-- Blizzard
-- Thunderbolt
-- Body Slam
-- Confuse Ray
-"""
-
 
 async def train():
-    agent = Agent(battle_format="gen1ou", team=sample_team)
-    random = RandomPlayer(battle_format="gen1ou", team=sample_team)
-    max_damage = MaxBasePowerPlayer(battle_format="gen1ou", team=sample_team)
-    simple_heuristic = SimpleHeuristicsPlayer(battle_format="gen1ou", team=sample_team)
+    agent = Agent(battle_format="gen4randombattle")
+    random = RandomPlayer(battle_format="gen4randombattle")
+    max_damage = MaxBasePowerPlayer(battle_format="gen4randombattle")
+    simple_heuristic = SimpleHeuristicsPlayer(battle_format="gen4randombattle")
     result = await cross_evaluate([agent, random, max_damage, simple_heuristic], n_challenges=10)
     print(result)
 
