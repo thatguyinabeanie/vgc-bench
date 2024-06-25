561e324c517a6ac389d11427c014aad6e708710f
Cameron Angliss <cameronangliss99@gmail.com>
2024-06-25 16:06:26 -0700

    cleanup

--- src/train.py
+++ src/train.py
@@ -1,11 +1,11 @@
 import asyncio
 import os
+import time
 
 from poke_env import cross_evaluate
 from poke_env.player import MaxBasePowerPlayer, RandomPlayer, SimpleHeuristicsPlayer
 from stable_baselines3 import PPO
 from stable_baselines3.common.vec_env import DummyVecEnv
-import time
 
 from agent import Agent
 from env import ShowdownEnv, ShowdownVecEnvWrapper
@@ -71,16 +71,13 @@ Adamant Nature
 
 async def train():
     # setup
-    ppo = PPO(
-        "MlpPolicy", ShowdownEnv(RandomPlayer()), learning_rate=1e-5, tensorboard_log="output/logs/ppo"
-    )
+    dummy_opponent = RandomPlayer(battle_format=BATTLE_FORMAT, team=TEAM)
+    env = ShowdownEnv(dummy_opponent, battle_format=BATTLE_FORMAT, log_level=40, team=TEAM)
+    vec_env = ShowdownVecEnvWrapper(DummyVecEnv([lambda: env]), env)
+    ppo = PPO("MlpPolicy", vec_env, learning_rate=1e-5, tensorboard_log="output/logs/ppo")
     if os.path.exists("output/saves/ppo.zip"):
         ppo.set_parameters("output/saves/ppo.zip")
         print("Resuming old run.")
-    opponent = Agent(ppo.policy, battle_format=BATTLE_FORMAT, team=TEAM)
-    env = ShowdownEnv(opponent, battle_format=BATTLE_FORMAT, log_level=40, team=TEAM)
-    vec_env = ShowdownVecEnvWrapper(DummyVecEnv([lambda: env]), env)
-    ppo.set_env(vec_env)
     random = RandomPlayer(battle_format=BATTLE_FORMAT, team=TEAM)
     max_power = MaxBasePowerPlayer(battle_format=BATTLE_FORMAT, team=TEAM)
     heuristics = SimpleHeuristicsPlayer(battle_format=BATTLE_FORMAT, team=TEAM)
