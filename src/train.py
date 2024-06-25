b4ebda0f7ca4470a3431da7a69aeb06c574438a9
Cameron Angliss <cameronangliss99@gmail.com>
2024-06-24 19:12:36 -0700

    simplify problem to simply overfitting on a constant team and opponent team

--- src/train.py
+++ src/train.py
@@ -1,38 +1,133 @@
 import asyncio
 import os
 
-from poke_env import cross_evaluate
-from poke_env.player import MaxBasePowerPlayer, RandomPlayer, SimpleHeuristicsPlayer
+from poke_env.player import SimpleHeuristicsPlayer
 from stable_baselines3 import PPO
 
-from agent import Agent
 from env import ShowdownEnv
 
-BATTLE_FORMAT = "gen4randombattle"
+BATTLE_FORMAT = "gen4ou"
+TEAM1 = """
+Bronzong @ Lum Berry
+Ability: Heatproof
+EVs: 248 HP / 252 Atk / 4 Def / 4 SpD
+Brave Nature
+IVs: 0 Spe
+- Stealth Rock
+- Gyro Ball
+- Earthquake
+- Explosion
+
+Gengar @ Life Orb
+Ability: Levitate
+EVs: 4 Atk / 252 SpA / 252 Spe
+Naive Nature
+- Shadow Ball
+- Focus Blast
+- Explosion
+- Hidden Power [Fire]
+
+Tyranitar (M) @ Choice Band
+Ability: Sand Stream
+EVs: 252 Atk / 44 SpD / 212 Spe
+Adamant Nature
+- Stone Edge
+- Crunch
+- Pursuit
+- Superpower
+
+Kingdra @ Choice Specs
+Ability: Swift Swim
+EVs: 252 SpA / 4 SpD / 252 Spe
+Modest Nature
+IVs: 0 Atk
+- Hydro Pump
+- Draco Meteor
+- Surf
+- Dragon Pulse
+
+Flygon (M) @ Choice Scarf
+Ability: Levitate
+EVs: 4 HP / 252 Atk / 252 Spe
+Jolly Nature
+- U-turn
+- Outrage
+- Earthquake
+- Thunder Punch
+
+Lucario @ Life Orb
+Ability: Inner Focus
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Close Combat
+- Swords Dance
+- Bullet Punch
+- Extreme Speed"""
+TEAM2 = """
+Bronzong @ Lum Berry
+Ability: Heatproof
+EVs: 248 HP / 252 Atk / 8 SpD
+Brave Nature
+IVs: 0 Spe
+- Gyro Ball
+- Stealth Rock
+- Earthquake
+- Explosion
+
+Dragonite (M) @ Choice Band
+Ability: Inner Focus
+EVs: 48 HP / 252 Atk / 208 Spe
+Adamant Nature
+- Outrage
+- Dragon Claw
+- Extreme Speed
+- Earthquake
+
+Mamoswine (M) @ Life Orb
+Ability: Oblivious
+EVs: 252 Atk / 4 Def / 252 Spe
+Jolly Nature
+- Ice Shard
+- Earthquake
+- Stone Edge
+- Superpower
+
+Magnezone @ Leftovers
+Ability: Magnet Pull
+EVs: 140 HP / 252 SpA / 116 Spe
+Modest Nature
+IVs: 2 Atk / 30 SpA / 30 Spe
+- Thunderbolt
+- Thunder Wave
+- Substitute
+- Hidden Power [Fire]
+
+Flygon @ Choice Scarf
+Ability: Levitate
+EVs: 252 Atk / 6 Def / 252 Spe
+Adamant Nature
+- Outrage
+- Earthquake
+- Stone Edge
+- U-turn
+
+Kingdra (M) @ Chesto Berry
+Ability: Swift Swim
+EVs: 144 HP / 160 Atk / 40 SpD / 164 Spe
+Adamant Nature
+- Dragon Dance
+- Waterfall
+- Outrage
+- Rest"""
 
 
 async def train():
-    # setup
-    ppo = PPO(
-        "MlpPolicy",
-        ShowdownEnv(RandomPlayer()),
-        learning_rate=lr_schedule,
-        tensorboard_log="output/logs/ppo",
-    )
+    opponent = SimpleHeuristicsPlayer(battle_format=BATTLE_FORMAT, team=TEAM2)
+    env = ShowdownEnv(opponent, battle_format=BATTLE_FORMAT, log_level=40, team=TEAM1)
+    ppo = PPO("MlpPolicy", env, learning_rate=lr_schedule, tensorboard_log="output/logs/ppo")
     if os.path.exists("output/saves/ppo"):
         ppo = ppo.load("output/saves/ppo")
-    opponent = Agent(ppo.policy, battle_format=BATTLE_FORMAT)
-    env = ShowdownEnv(opponent, battle_format=BATTLE_FORMAT, log_level=40)
-    ppo.set_env(env)
-    # train
-    ppo = ppo.learn(1_000_000, reset_num_timesteps=False, progress_bar=True)
-    # evaluate
-    agent = Agent(ppo.policy, battle_format=BATTLE_FORMAT)
-    random = RandomPlayer(battle_format=BATTLE_FORMAT)
-    max_damage = MaxBasePowerPlayer(battle_format=BATTLE_FORMAT)
-    simple_heuristic = SimpleHeuristicsPlayer(battle_format=BATTLE_FORMAT)
-    result = await cross_evaluate([agent, random, max_damage, simple_heuristic], n_challenges=10)
-    print(result[agent.username])
+    ppo = ppo.learn(1_000_000, progress_bar=True)
     ppo.save("output/saves/ppo")
 
 
