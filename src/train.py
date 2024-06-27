b334845c1c22056328a3705bf108b23729658dde
Cameron Angliss <cameronangliss99@gmail.com>
2024-06-27 01:41:04 -0700

    bugfixing/refactoring

--- src/train.py
+++ src/train.py
@@ -70,11 +70,11 @@ TOTAL_TIMESTEPS = 100_000_000
 
 
 class SaveAndReplaceOpponentCallback(BaseCallback):
-    def __init__(self, rollout_len: int, save_freq: int, rollouts: int):
+    def __init__(self, save_freq: int, num_saved_timesteps: int, n_steps: int):
         super().__init__()
-        self.rollout_len = rollout_len
         self.save_freq = save_freq
-        self.rollout_count = rollouts
+        self.num_timesteps = num_saved_timesteps
+        self.n_steps = n_steps
 
     def _on_step(self) -> bool:
         return True
@@ -82,9 +82,9 @@ class SaveAndReplaceOpponentCallback(BaseCallback):
     def on_rollout_end(self):
         opponent = Agent(self.model.policy, battle_format=BATTLE_FORMAT, team=TEAM)
         self.model.env.set_opponent(opponent)  # type: ignore
-        self.rollout_count += 1
-        if self.rollout_count % self.save_freq == 0:
-            self.model.save(f"output/saves/ppo_{self.rollout_len * self.rollout_count}")
+        self.num_timesteps += self.n_steps
+        if self.num_timesteps % self.save_freq == 0:
+            self.model.save(f"output/saves/ppo_{self.num_timesteps}")
 
 
 async def train():
@@ -93,29 +93,25 @@ async def train():
     env = ShowdownEnv(opponent, battle_format=BATTLE_FORMAT, log_level=40, team=TEAM)
     wrapper_env = ShowdownVecEnvWrapper(DummyVecEnv([lambda: env]), env)
     ppo = PPO("MlpPolicy", wrapper_env, tensorboard_log="output/logs/ppo")
-    num_saved_rollouts = 0
+    num_saved_timesteps = 0
     if os.path.exists("output/saves") and len(os.listdir("output/saves")) > 0:
         files = os.listdir("output/saves")
-        num_saved_rollouts = max([int(file[4:-4]) for file in files])
-        ppo.set_parameters(f"output/saves/ppo_{num_saved_rollouts}.zip")
-        print(f"Resuming ppo_{num_saved_rollouts}.zip run.")
+        num_saved_timesteps = max([int(file[4:-4]) for file in files])
+        ppo.set_parameters(f"output/saves/ppo_{num_saved_timesteps}.zip")
+        print(f"Resuming ppo_{num_saved_timesteps}.zip run.")
     opponent = Agent(ppo.policy, battle_format=BATTLE_FORMAT, team=TEAM)
     ppo.env.set_opponent(opponent)  # type: ignore
 
     def calc_learning_rate(progress_remaining: float) -> float:
         progress = 1 - progress_remaining
-        saved_progress = num_saved_rollouts / TOTAL_TIMESTEPS
+        saved_progress = num_saved_timesteps / TOTAL_TIMESTEPS
         return 10**-4.23 / (8 * (progress + saved_progress) / (1 + saved_progress) + 1) ** 1.5
 
     ppo.learning_rate = calc_learning_rate
     # train
-    callback = SaveAndReplaceOpponentCallback(
-        rollout_len=ppo.n_steps, save_freq=50, rollouts=num_saved_rollouts
-    )
+    callback = SaveAndReplaceOpponentCallback(102_400, num_saved_timesteps, ppo.n_steps)
     ppo = ppo.learn(
-        TOTAL_TIMESTEPS - num_saved_rollouts * ppo.n_steps,
-        callback=callback,
-        reset_num_timesteps=False,
+        TOTAL_TIMESTEPS - num_saved_timesteps, callback=callback, reset_num_timesteps=False
     )
     # evaluate
     agent = Agent(ppo.policy, battle_format=BATTLE_FORMAT, team=TEAM)
