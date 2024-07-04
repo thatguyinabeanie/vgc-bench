36168d26d09f661e6944b9d69d5e6734f317c3f4
Cameron Angliss <cameronangliss99@gmail.com>
2024-07-04 14:26:05 -0700

    bugfix and code cleanup

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ from typing import Any
 import torch
 from poke_env.player import MaxBasePowerPlayer, Player, RandomPlayer, SimpleHeuristicsPlayer
 from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.distributions import Distribution
 from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.type_aliases import PyTorchObs
 
@@ -18,25 +19,9 @@ class MaskedActorCriticPolicy(ActorCriticPolicy):
     def forward(
         self, obs: torch.Tensor, deterministic: bool = True
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        bool_mask = ~obs[:, :10].bool()
-        features = self.extract_features(obs)
-        if self.share_features_extractor:
-            latent_pi, latent_vf = self.mlp_extractor(features)
-        else:
-            pi_features, vf_features = features
-            latent_pi = self.mlp_extractor.forward_actor(pi_features)
-            latent_vf = self.mlp_extractor.forward_critic(vf_features)
-        values = self.value_net(latent_vf)
-        mean_actions = self.action_net(latent_pi)
-        mask = torch.zeros(bool_mask.size()).to(self.device)
-        for i in range(mask.size(0)):
-            mask[i] = (
-                mask[i]
-                if bool_mask[i].all()
-                else mask[i].masked_fill(bool_mask[i], torch.min(mean_actions[i]) - 1)
-            )
-        distribution = self.action_dist.proba_distribution(action_logits=mean_actions + mask)
+        distribution = self.get_distribution(obs)
         actions = distribution.get_actions(deterministic=deterministic)
+        values = self.predict_values(obs)
         log_prob = distribution.log_prob(actions)
         actions = actions.reshape((-1, *self.action_space.shape))  # type: ignore[misc]
         return actions, values, log_prob
@@ -44,27 +29,31 @@ class MaskedActorCriticPolicy(ActorCriticPolicy):
     def evaluate_actions(
         self, obs: PyTorchObs, actions: torch.Tensor
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor | None]:
+        distribution = self.get_distribution(obs)
+        values = self.predict_values(obs)
+        log_prob = distribution.log_prob(actions)
+        entropy = distribution.entropy()
+        return values, log_prob, entropy
+
+    def get_distribution(self, obs: PyTorchObs) -> Distribution:
         bool_mask = ~obs[:, :10].bool()  # type: ignore
         features = self.extract_features(obs)
         if self.share_features_extractor:
-            latent_pi, latent_vf = self.mlp_extractor(features)
+            latent_pi, _ = self.mlp_extractor(features)
         else:
-            pi_features, vf_features = features
+            pi_features, _ = features
             latent_pi = self.mlp_extractor.forward_actor(pi_features)
-            latent_vf = self.mlp_extractor.forward_critic(vf_features)
         mean_actions = self.action_net(latent_pi)
         mask = torch.zeros(bool_mask.size()).to(self.device)
         for i in range(mask.size(0)):
             mask[i] = (
                 mask[i]
                 if bool_mask[i].all()
-                else mask[i].masked_fill(bool_mask[i], torch.min(mean_actions[i]) - 1)
+                else mask[i].masked_fill(
+                    bool_mask[i], -(torch.max(mean_actions[i]) - torch.min(mean_actions[i]) + 1)
+                )
             )
-        distribution = self.action_dist.proba_distribution(action_logits=mean_actions + mask)
-        log_prob = distribution.log_prob(actions)
-        values = self.value_net(latent_vf)
-        entropy = distribution.entropy()
-        return values, log_prob, entropy
+        return self.action_dist.proba_distribution(action_logits=mean_actions + mask)
 
 
 class Callback(BaseCallback):
