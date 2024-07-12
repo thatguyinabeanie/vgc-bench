77fde38c070381e2ed198619ab17ee80b1b71f4c
Cameron Angliss <cameronangliss99@gmail.com>
2024-07-12 01:11:37 -0700

    better behavior

--- src/train.py
+++ src/train.py
@@ -1,19 +1,5 @@
 import asyncio
-import os
-from subprocess import DEVNULL, Popen
-
-import nest_asyncio
-from poke_env import AccountConfiguration
-from stable_baselines3 import PPO
-from stable_baselines3.common.vec_env import DummyVecEnv
-
-from agent import Agent
-from callback import Callback
-from env import ShowdownEnv
-from policy import MaskedActorCriticPolicy
-from teams import TEAM1
-
-nest_asyncio.apply()
+from subprocess import DEVNULL, Popen, call
 
 
 async def train_forever():
@@ -25,58 +11,9 @@ async def train_forever():
             cwd="pokemon-showdown",
         )
         await asyncio.sleep(5)
-        train()
+        call(["python", "src/train_step.py"])
         process.terminate()
         process.wait()
-
-
-def train():
-    total_steps = 10_000_000
-    steps = 102_400
-    num_envs = 8
-    battle_format = "gen4ou"
-    opponents = [
-        Agent(
-            None,
-            account_configuration=AccountConfiguration(f"Opponent{i + 1}", None),
-            battle_format=battle_format,
-            log_level=40,
-            team=TEAM1,
-        )
-        for i in range(num_envs)
-    ]
-    env = DummyVecEnv(
-        [
-            lambda i=i: ShowdownEnv(
-                opponents[i],
-                account_configuration=AccountConfiguration(f"Agent{i + 1}", None),
-                battle_format=battle_format,
-                log_level=40,
-                team=TEAM1,
-            )
-            for i in range(num_envs)
-        ]
-    )
-    num_saved_timesteps = 0
-    if os.path.exists("output/saves") and len(os.listdir("output/saves")) > 0:
-        files = os.listdir("output/saves")
-        num_saved_timesteps = max([int(file[4:-4]) for file in files])
-
-    def calc_learning_rate(progress_remaining: float) -> float:
-        progress = (1 - progress_remaining) * (num_saved_timesteps + steps) / total_steps
-        return 1e-4 / (8 * progress + 1) ** 1.5
-
-    ppo = PPO(
-        MaskedActorCriticPolicy,
-        env,
-        learning_rate=calc_learning_rate,
-        n_steps=2048 // num_envs,
-        tensorboard_log="output/logs/ppo",
-    )
-    if num_saved_timesteps > 0:
-        ppo.set_parameters(os.path.join("output/saves", f"ppo_{num_saved_timesteps}.zip"))
-    callback = Callback(opponents, num_saved_timesteps, steps, battle_format, TEAM1)
-    ppo = ppo.learn(num_saved_timesteps + steps, callback=callback, reset_num_timesteps=False)
 
 
 if __name__ == "__main__":
---
+++ src/train_step.py
@@ -0,0 +1,64 @@
+import os
+
+from poke_env import AccountConfiguration
+from stable_baselines3 import PPO
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+from agent import Agent
+from callback import Callback
+from env import ShowdownEnv
+from policy import MaskedActorCriticPolicy
+from teams import TEAM1
+
+
+def train_step():
+    total_steps = 10_000_000
+    steps = 102_400
+    num_envs = 8
+    battle_format = "gen4ou"
+    opponents = [
+        Agent(
+            None,
+            account_configuration=AccountConfiguration(f"Opponent{i + 1}", None),
+            battle_format=battle_format,
+            log_level=40,
+            team=TEAM1,
+        )
+        for i in range(num_envs)
+    ]
+    env = DummyVecEnv(
+        [
+            lambda i=i: ShowdownEnv(
+                opponents[i],
+                account_configuration=AccountConfiguration(f"Agent{i + 1}", None),
+                battle_format=battle_format,
+                log_level=40,
+                team=TEAM1,
+            )
+            for i in range(num_envs)
+        ]
+    )
+    num_saved_timesteps = 0
+    if os.path.exists("output/saves") and len(os.listdir("output/saves")) > 0:
+        files = os.listdir("output/saves")
+        num_saved_timesteps = max([int(file[4:-4]) for file in files])
+
+    def calc_learning_rate(progress_remaining: float) -> float:
+        progress = (1 - progress_remaining) * (num_saved_timesteps + steps) / total_steps
+        return 1e-4 / (8 * progress + 1) ** 1.5
+
+    ppo = PPO(
+        MaskedActorCriticPolicy,
+        env,
+        learning_rate=calc_learning_rate,
+        n_steps=2048 // num_envs,
+        tensorboard_log="output/logs/ppo",
+    )
+    if num_saved_timesteps > 0:
+        ppo.set_parameters(os.path.join("output/saves", f"ppo_{num_saved_timesteps}.zip"))
+    callback = Callback(opponents, num_saved_timesteps, steps, battle_format, TEAM1)
+    ppo = ppo.learn(num_saved_timesteps + steps, callback=callback, reset_num_timesteps=False)
+
+
+if __name__ == "__main__":
+    train_step()
