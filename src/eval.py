8dc1fedd2f9c244be43312bbd00d9200fdada02b
Cameron Angliss <cameronangliss99@gmail.com>
2024-08-07 16:21:05 -0700

    add eval file

---
+++ src/eval.py
@@ -0,0 +1,73 @@
+import asyncio
+import os
+import time
+from subprocess import DEVNULL, Popen
+
+from poke_env import AccountConfiguration, ServerConfiguration
+from poke_env.player import MaxBasePowerPlayer, Player, RandomPlayer, SimpleHeuristicsPlayer
+from stable_baselines3 import PPO
+
+from agent import Agent
+
+
+def run_eval():
+    Popen(
+        ["node", "pokemon-showdown", "start", "--no-security"],
+        stdout=DEVNULL,
+        stderr=DEVNULL,
+        cwd="pokemon-showdown",
+    )
+    time.sleep(5)
+    num_saved_timesteps = 0
+    if os.path.exists("saves") and len(os.listdir("saves")) > 0:
+        files = os.listdir("saves")
+        num_saved_timesteps = max([int(file[:-4]) for file in files])
+    policy = PPO.load(f"saves/{num_saved_timesteps}.zip").policy
+    eval_agent = Agent(
+        policy,
+        account_configuration=AccountConfiguration("Agent", None),
+        server_configuration=ServerConfiguration(
+            "ws://localhost:9000/showdown/websocket", "https://play.pokemonshowdown.com/action.php?"
+        ),
+        battle_format="gen9ou",
+        log_level=40,
+        max_concurrent_battles=10,
+    )
+    eval_opponents: list[Player] = [
+        RandomPlayer(
+            account_configuration=AccountConfiguration("Random", None),
+            server_configuration=ServerConfiguration(
+                "ws://localhost:9000/showdown/websocket",
+                "https://play.pokemonshowdown.com/action.php?",
+            ),
+            battle_format="gen9ou",
+            log_level=40,
+            max_concurrent_battles=10,
+        ),
+        MaxBasePowerPlayer(
+            account_configuration=AccountConfiguration("Power", None),
+            server_configuration=ServerConfiguration(
+                "ws://localhost:9000/showdown/websocket",
+                "https://play.pokemonshowdown.com/action.php?",
+            ),
+            battle_format="gen9ou",
+            log_level=40,
+            max_concurrent_battles=10,
+        ),
+        SimpleHeuristicsPlayer(
+            account_configuration=AccountConfiguration("Heuristics", None),
+            server_configuration=ServerConfiguration(
+                "ws://localhost:9000/showdown/websocket",
+                "https://play.pokemonshowdown.com/action.php?",
+            ),
+            battle_format="gen9ou",
+            log_level=40,
+            max_concurrent_battles=10,
+        ),
+    ]
+    result = asyncio.run(eval_agent.battle_against_multi(eval_opponents, n_battles=100))
+    print(result)
+
+
+if __name__ == "__main__":
+    run_eval()
