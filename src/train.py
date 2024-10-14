fde329750d9396b9f71d0c0948286699d5a1c48c
Cameron Angliss <cameronangliss99@gmail.com>
2024-10-13 22:46:02 -0500

    unused import

--- src/train.py
+++ src/train.py
@@ -4,7 +4,6 @@ import os
 import time
 from subprocess import DEVNULL, Popen
 
-import torch
 from stable_baselines3 import PPO
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.vec_env import SubprocVecEnv
