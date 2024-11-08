0ea7fa48da32ea6bb7ef04d7bf1fb9417f02f84d
Cameron Angliss <cameronangliss99@gmail.com>
2024-11-08 12:34:27 -0600

    update run to not destroy computer

--- run.sh
+++ run.sh
@@ -2,8 +2,4 @@
 if [[ ":$PATH:" != "/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:"* ]]; then
     export PATH="/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:$PATH"
 fi
-python src/train.py --teams 0 --opp_teams 0 1 2 3 --port 8000 --device cuda:0 &
-python src/train.py --teams 0 --opp_teams 0 1 2 3 4 5 6 7 --port 8001 --device cuda:1 &
-python src/train.py --teams 0 --opp_teams 0 1 2 3 4 5 6 7 8 9 10 11 --port 8002 --device cuda:2 &
-python src/train.py --teams 0 --opp_teams 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 --port 8003 --device cuda:3 &
-wait
+python src/train.py --num_teams 16 --port 8000 --device cuda:0
