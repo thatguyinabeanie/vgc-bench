d1a78aa38989ee85443277f73e2af8032ea7949d
Cameron Angliss <cameronangliss99@gmail.com>
2024-11-08 21:22:56 -0600

    add the debug.log redirect

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 if [[ ":$PATH:" != "/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:"* ]]; then
     export PATH="/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:$PATH"
 fi
-python src/train.py --num_teams 16 --port 8000 --device cuda:0
+python src/train.py --num_teams 16 --port 8000 --device cuda:0 &> debug.log
