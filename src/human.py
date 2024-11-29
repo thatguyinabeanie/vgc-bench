ef151469e6b54e1d7c861cb63cf630f446fb3f25
Cameron Angliss <cameronangliss99@gmail.com>
2024-11-29 13:29:50 -0600

    adding print feedback, since the data scraping takes a while

--- src/human.py
+++ src/human.py
@@ -5,14 +5,18 @@ import requests
 
 
 def scrape():
+    print("Scraping replay data from pages 1-100")
     battle_jsons = [
         battle_json
         for page in range(1, 101)
         for battle_json in get_battle_jsons(page)
         if battle_json["rating"] is None
     ]
+    print(f"Retrieved {len(battle_jsons)} battle header jsons")
     log_jsons = [get_log_json(battle_json["id"][16:]) for battle_json in battle_jsons]
+    print(f"Converted to {len(log_jsons)} battle logs")
     logs = [lj["log"] for lj in log_jsons if "|showteam|" in lj["log"]]
+    print(f"Saving {len(logs)} battle logs with open sheets to json/human.json")
     with open("json/human.json", "w") as f:
         json.dump(logs, f)
 
