8e94aabef308426830cf68ce11e76fb4d63d7847
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-10 02:49:18 -0600

    increase batch size, frame stack during BC, log BC with tensorboard

--- dexter/train.py
+++ dexter/train.py
@@ -5,6 +5,8 @@ from subprocess import PIPE, STDOUT, Popen
 
 import numpy as np
 from imitation.algorithms.bc import BC
+from imitation.data.types import Trajectory
+from imitation.util.logger import configure
 from poke_env import AccountConfiguration
 from poke_env.player import RandomPlayer
 from src.callback import Callback
@@ -45,19 +47,37 @@ def pretrain():
         start_listening=False,
     )
     ppo = PPO(MaskedActorCriticPolicy, env, policy_kwargs={"num_frames": num_frames}, device=device)
-    print("loading trajectories...", flush=True)
     with open("data/trajs.pkl", "rb") as f:
-        trajs = pickle.load(f)
-    print(f"loaded {len(trajs)} trajectories", flush=True)
+        trajs: list[Trajectory] = pickle.load(f)
+    trajs = [
+        Trajectory(
+            obs=np.stack(
+                [
+                    np.stack(
+                        [traj.obs[max(j, 0)] for j in range(i - num_frames + 1, i + 1)],  # type: ignore
+                        axis=0,
+                    )
+                    for i in range(len(traj.obs))
+                ],
+                axis=0,
+            ),
+            acts=traj.acts,
+            infos=None,
+            terminal=True,
+        )
+        for traj in trajs
+    ]
     bc = BC(
         observation_space=ppo.observation_space,
         action_space=ppo.action_space,
         rng=np.random.default_rng(0),
         policy=ppo.policy,
         demonstrations=trajs,
+        batch_size=1024,
         device=device,
+        custom_logger=configure(f"logs/{run_name}", ["tensorboard"]),
     )
-    bc.train(n_epochs=10)
+    bc.train(n_epochs=100)
     ppo.save(f"saves/{run_name}/0")
 
 
