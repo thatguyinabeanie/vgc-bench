87aa6c4a336ff660ab58611e07bae1ed3d4f8f14
Cameron Angliss <cameronangliss99@gmail.com>
2024-12-14 14:44:38 -0600

    cleanup

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ ":$PATH:" != *"/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:"* ]]; then
-  export PATH="/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:$PATH"
+if [[ $PATH != "/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:"* ]]; then
+    export PATH="/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:$PATH"
 fi
 
 python dexter/pretrain.py
--- update_path.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-if [[ $PATH != "/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:"* ]]; then
-    export PATH="/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:$PATH"
-fi
