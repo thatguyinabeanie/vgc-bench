70f62f9a714665ed5485978678a0107bf89fb7a4
Cameron Angliss <cameronangliss99@gmail.com>
2025-02-10 05:02:27 -0600

    don't fail silently

--- run.sh
+++ run.sh
@@ -4,7 +4,4 @@ if [[ $PATH != "/scratch/cluster/cangliss/repos/UT-masters-thesis/bin:"* ]]; the
 fi
 
 # python dexter/pretrain.py
-while true; do
-    python dexter/train.py > debug.log 2>&1
-    [[ $? -ne 0 ]] && sleep 10
-done
+while python dexter/train.py > debug.log 2>&1; do :; done
