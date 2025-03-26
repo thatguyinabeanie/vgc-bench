549d1028dc8bcb3961442f8574e6d6ec3889aa8d
Cameron Angliss <cameronangliss99@gmail.com>
2025-03-26 18:51:50 -0500

    pretrain script

---
+++ pretrain.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [[ $PATH != "/scratch/cluster/cangliss/bin:"* ]]; then
+    export PATH="/scratch/cluster/cangliss/bin:$PATH"
+fi
+
+num_teams=1
+port=8000
+device="cuda:0"
+
+start_showdown() {
+    local port=$1
+    (
+        cd pokemon-showdown-"$port"
+        node pokemon-showdown start "$port" --no-security > /dev/null 2>&1 &
+        echo $!
+    )
+}
+
+echo "Starting Showdown server for pretraining process..."
+showdown_pid=$(start_showdown "$port")
+echo "Starting pretraining process..."
+python dexter/pretrain.py --num_teams "$num_teams" --port "$port" --device "$device"
+exit_status=$?
+if [ $exit_status -ne 0 ]; then
+    echo "Pretraining process died with exit status $exit_status"
+fi
+kill $showdown_pid
