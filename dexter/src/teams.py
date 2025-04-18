1f0505755100d26641f9990abd9500ac18e1bc62
Cameron Angliss <cameronangliss99@gmail.com>
2025-04-17 20:49:10 -0500

    prepping evaluation of popular matches

--- dexter/src/teams.py
+++ dexter/src/teams.py
@@ -2838,6 +2838,145 @@ IVs: 0 Atk
 - Icy Wind
 """,
         """
+Miraidon @ Choice Specs
+Ability: Hadron Engine
+Level: 50
+Tera Type: Fairy
+EVs: 44 HP / 4 Def / 244 SpA / 12 SpD / 204 Spe
+Modest Nature
+- Electro Drift
+- Draco Meteor
+- Volt Switch
+- Dazzling Gleam
+
+Whimsicott @ Covert Cloak
+Ability: Prankster
+Level: 50
+Tera Type: Dark
+EVs: 236 HP / 164 SpD / 108 Spe
+Timid Nature
+IVs: 0 Atk
+- Moonblast
+- Tailwind
+- Light Screen
+- Encore
+
+Urshifu-Rapid-Strike @ Focus Sash
+Ability: Unseen Fist
+Level: 50
+Tera Type: Stellar
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Surging Strikes
+- Close Combat
+- Aqua Jet
+- Protect
+
+Ogerpon-Hearthflame (F) @ Hearthflame Mask
+Ability: Mold Breaker
+Level: 50
+Tera Type: Fire
+EVs: 188 HP / 76 Atk / 52 Def / 4 SpD / 188 Spe
+Adamant Nature
+- Ivy Cudgel
+- Wood Hammer
+- Follow Me
+- Spiky Shield
+
+Farigiraf @ Electric Seed
+Ability: Armor Tail
+Level: 50
+Tera Type: Water
+EVs: 204 HP / 164 Def / 4 SpA / 108 SpD / 28 Spe
+Bold Nature
+IVs: 6 Atk
+- Foul Play
+- Psychic Noise
+- Trick Room
+- Helping Hand
+
+Iron Hands @ Assault Vest
+Ability: Quark Drive
+Level: 50
+Tera Type: Bug
+EVs: 76 HP / 180 Atk / 12 Def / 236 SpD
+Brave Nature
+IVs: 0 Spe
+- Drain Punch
+- Low Kick
+- Wild Charge
+- Fake Out""",
+        """
+Calyrex-Ice @ Clear Amulet
+Ability: As One (Glastrier)
+Level: 50
+Tera Type: Grass
+EVs: 252 HP / 172 Atk / 84 SpD
+Brave Nature
+IVs: 1 Spe
+- Glacial Lance
+- High Horsepower
+- Protect
+- Trick Room
+
+Urshifu-Rapid-Strike (M) @ Focus Sash
+Ability: Unseen Fist
+Level: 50
+Tera Type: Water
+EVs: 252 Atk / 4 SpD / 252 Spe
+Adamant Nature
+- Surging Strikes
+- Close Combat
+- Aqua Jet
+- Detect
+
+Pelipper @ Life Orb
+Ability: Drizzle
+Level: 50
+Tera Type: Grass
+EVs: 252 HP / 252 SpA / 4 SpD
+Modest Nature
+IVs: 0 Atk
+- Weather Ball
+- Hurricane
+- Helping Hand
+- Wide Guard
+
+Amoonguss (M) @ Rocky Helmet
+Ability: Regenerator
+Level: 50
+Tera Type: Fire
+EVs: 236 HP / 228 Def / 44 SpD
+Relaxed Nature
+IVs: 0 Atk / 0 Spe
+- Spore
+- Rage Powder
+- Clear Smog
+- Pollen Puff
+
+Iron Valiant @ Booster Energy
+Ability: Quark Drive
+Level: 50
+Shiny: Yes
+Tera Type: Ghost
+EVs: 204 HP / 4 Atk / 100 Def / 28 SpD / 172 Spe
+Jolly Nature
+- Spirit Break
+- Coaching
+- Wide Guard
+- Encore
+
+Landorus @ Choice Scarf
+Ability: Sheer Force
+Level: 50
+Tera Type: Ghost
+EVs: 4 HP / 252 SpA / 252 Spe
+Modest Nature
+- Earth Power
+- Sandsear Storm
+- Sludge Bomb
+- U-turn""",
+        """
 Flutter Mane @ Focus Sash
 Ability: Protosynthesis
 Level: 50
@@ -2979,145 +3118,6 @@ IVs: 20 Atk / 13 Spe
 - Roar
 - Trick Room
 - Helping Hand""",
-        """
-Miraidon @ Choice Specs
-Ability: Hadron Engine
-Level: 50
-Tera Type: Fairy
-EVs: 44 HP / 4 Def / 244 SpA / 12 SpD / 204 Spe
-Modest Nature
-- Electro Drift
-- Draco Meteor
-- Volt Switch
-- Dazzling Gleam
-
-Whimsicott @ Covert Cloak
-Ability: Prankster
-Level: 50
-Tera Type: Dark
-EVs: 236 HP / 164 SpD / 108 Spe
-Timid Nature
-IVs: 0 Atk
-- Moonblast
-- Tailwind
-- Light Screen
-- Encore
-
-Urshifu-Rapid-Strike @ Focus Sash
-Ability: Unseen Fist
-Level: 50
-Tera Type: Stellar
-EVs: 252 Atk / 4 SpD / 252 Spe
-Adamant Nature
-- Surging Strikes
-- Close Combat
-- Aqua Jet
-- Protect
-
-Ogerpon-Hearthflame (F) @ Hearthflame Mask
-Ability: Mold Breaker
-Level: 50
-Tera Type: Fire
-EVs: 188 HP / 76 Atk / 52 Def / 4 SpD / 188 Spe
-Adamant Nature
-- Ivy Cudgel
-- Wood Hammer
-- Follow Me
-- Spiky Shield
-
-Farigiraf @ Electric Seed
-Ability: Armor Tail
-Level: 50
-Tera Type: Water
-EVs: 204 HP / 164 Def / 4 SpA / 108 SpD / 28 Spe
-Bold Nature
-IVs: 6 Atk
-- Foul Play
-- Psychic Noise
-- Trick Room
-- Helping Hand
-
-Iron Hands @ Assault Vest
-Ability: Quark Drive
-Level: 50
-Tera Type: Bug
-EVs: 76 HP / 180 Atk / 12 Def / 236 SpD
-Brave Nature
-IVs: 0 Spe
-- Drain Punch
-- Low Kick
-- Wild Charge
-- Fake Out""",
-        """
-Calyrex-Ice @ Clear Amulet
-Ability: As One (Glastrier)
-Level: 50
-Tera Type: Grass
-EVs: 252 HP / 172 Atk / 84 SpD
-Brave Nature
-IVs: 1 Spe
-- Glacial Lance
-- High Horsepower
-- Protect
-- Trick Room
-
-Urshifu-Rapid-Strike (M) @ Focus Sash
-Ability: Unseen Fist
-Level: 50
-Tera Type: Water
-EVs: 252 Atk / 4 SpD / 252 Spe
-Adamant Nature
-- Surging Strikes
-- Close Combat
-- Aqua Jet
-- Detect
-
-Pelipper @ Life Orb
-Ability: Drizzle
-Level: 50
-Tera Type: Grass
-EVs: 252 HP / 252 SpA / 4 SpD
-Modest Nature
-IVs: 0 Atk
-- Weather Ball
-- Hurricane
-- Helping Hand
-- Wide Guard
-
-Amoonguss (M) @ Rocky Helmet
-Ability: Regenerator
-Level: 50
-Tera Type: Fire
-EVs: 236 HP / 228 Def / 44 SpD
-Relaxed Nature
-IVs: 0 Atk / 0 Spe
-- Spore
-- Rage Powder
-- Clear Smog
-- Pollen Puff
-
-Iron Valiant @ Booster Energy
-Ability: Quark Drive
-Level: 50
-Shiny: Yes
-Tera Type: Ghost
-EVs: 204 HP / 4 Atk / 100 Def / 28 SpD / 172 Spe
-Jolly Nature
-- Spirit Break
-- Coaching
-- Wide Guard
-- Encore
-
-Landorus @ Choice Scarf
-Ability: Sheer Force
-Level: 50
-Tera Type: Ghost
-EVs: 4 HP / 252 SpA / 252 Spe
-Modest Nature
-- Earth Power
-- Sandsear Storm
-- Sludge Bomb
-- U-turn""",
     ],
     "gen9vgc2024regh": [
         """
--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@ if [[ $PATH != "/scratch/cluster/cangliss/bin:"* ]]; then
     export PATH="/scratch/cluster/cangliss/bin:$PATH"
 fi
 
-teams=(1 3 10 30)
-matchups=("30 31" "32 33" "30 32" "31 33")
+team_counts=(1 3 10 30)
+team_lists=("30 31" "32 33" "30")
 ports=(8000 8001 8002 8003)
 devices=("cuda:0" "cuda:1" "cuda:2" "cuda:3")
 
@@ -20,16 +20,15 @@ start_showdown() {
 
 start_training() {
     local i=$1
-    local team=${teams[$i]}
-    local a b
-    read -r a b <<< "${matchups[$i]}"
+    local num_teams=${team_counts[$i]}
+    local team_indices=${team_lists[$i]}
     local port=${ports[$i]}
     local device=${devices[$i]}
 
     echo "Starting Showdown server for training process $i..."
     showdown_pid=$(start_showdown "$port")
     echo "Starting training process $i..."
-    python dexter/train.py --teams "$a" "$b" --port "$port" --device "$device" --fictitious_play > debug"$port".log 2>&1
+    python dexter/train.py --num_teams "$num_teams" --port "$port" --device "$device" --behavior_clone --fictitious_play --num_frames 3 > debug"$port".log 2>&1
     exit_status=$?
     if [ $exit_status -ne 0 ]; then
         echo "Training process $i died with exit status $exit_status"
