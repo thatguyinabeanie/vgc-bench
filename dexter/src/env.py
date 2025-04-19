04af08084932dbdbd0d50026e993fdac3f341114
Cameron Angliss <cameronangliss99@gmail.com>
2025-04-19 06:00:52 -0500

    go back

--- dexter/src/env.py
+++ dexter/src/env.py
@@ -54,7 +54,6 @@ class ShowdownEnv(DoublesEnv[npt.NDArray[np.float32]]):
             accept_open_team_sheet=True,
             open_timeout=None,
             team=RandomTeamBuilder(teams, battle_format, toggle),
-            start_challenging=True,
             strict=False,
         )
         if learning_style == LearningStyle.PURE_SELF_PLAY:
